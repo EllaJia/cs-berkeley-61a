78ebb58f4886ef9d167c579d3bbe141baee311f8
Jia <jjia@dolby.com>
2023-11-22 17:24:11 -0800

    added problem background for take_pebbles.py

--- lectures/recursion/take_pebbles.py
+++ lectures/recursion/take_pebbles.py
@@ -1,3 +1,14 @@
+'''
+Problem:
+
+    Consider a two-player game in which there are n initial pebbles on a table. The players take turns, removing either one or two pebbles from the table, and the player who removes the final pebble wins. Suppose that Alice and Bob play this game, each using a simple strategy:
+
+    - Alice always removes a single pebble
+    - Bob removes two pebbles if an even number of pebbles is on the table, and one otherwise
+    
+    Given n initial pebbles and Alice starting, who wins the game?
+'''
+
 def alice(n):
     if n == 0 or n == 1:
         print("Alice wins!")
