5050771596b2f35b26c2cef4e4c22b02b15b2a3c
Jiaying Jia <ellen.jiaying.jia@gmail.com>
2024-04-23 23:09:19 -0700

    add additional practice in disc05

--- dis/disc05.py
+++ dis/disc05.py
@@ -108,3 +108,119 @@ def height(t):
     if is_leaf(t): return 0
     
     return 1 + max(height(i) for i in branches(t))
+
+'''
+Q6: Find Path
+    Write a function find_path that takes in a tree t with unique labels and a value x. It returns a list containing the labels of the nodes along the path from the root of t to the node labeled x.
+
+    If x is not a label in t, return None. Assume that the labels of t are unique.
+
+    For the following tree, find_path(t, 5) should return [2, 7, 6, 5].
+'''
+def find_path(t, x):
+    """
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])] ), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10)  # returns None
+    """
+    if label(t) == x:
+        return [label(t)]
+    for b in branches(t):
+        # In this way, we can go to the chilren first, and then decide if the parents will be added into the list
+        path = find_path(b, x)  # if there's a "x" in the branch, it will return, but if there's not, it's not gonna return anything
+        if path:
+            return [label(t)] + path
+        
+'''
+Q7: Sprout Leaves
+    Define a function sprout_leaves that takes in a tree, t, and a list of leaf labels, leaves. It produces a new tree that is identical to t, but where each old leaf node has new branches, for each label in leaves.
+'''
+def sprout_leaves(t, leaves):
+    """Sprout new leaves containing the data in leaves at each leaf in
+    the original tree t and return the resulting tree.
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return tree(t, [tree(val) for val in leaves])
+    return tree(label(t), [sprout_leaves(b, leaves) for b in branches(t)])
+
+'''
+Q8: Perfectly Balanced
+    Part A: Implement sum_tree, which returns the sum of all the labels in tree t.
+    Part B: Implement balanced, which returns whether every branch of t has the same total sum and that the branches themselves are also balanced.
+    
+    Hint: If we ever need to select a specific branch, we will need to break index into our branches list!
+
+    Challenge: Solve both of these parts with just 1 line of code each.
+'''
+def sum_tree(t):
+    """
+    Add all elements in a tree.
+    >>> t = tree(4, [tree(2, [tree(3)]), tree(6)])
+    >>> sum_tree(t)
+    15
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return label(t)
+    return label(t) + sum(sum_tree(b) for b in branches(t))
+
+
+def balanced(t):
+    """
+    Checks if each branch has same sum of all elements and
+    if each branch is balanced.
+    >>> t = tree(1, [tree(3), tree(1, [tree(2)]), tree(1, [tree(1), tree(1)])])
+    >>> balanced(t)
+    True
+    >>> t = tree(1, [t, tree(1)])
+    >>> balanced(t)
+    False
+    >>> t = tree(1, [tree(4), tree(1, [tree(2), tree(1)]), tree(1, [tree(3)])])
+    >>> balanced(t)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    num = len(branches(t))
+    sum_branch = 0
+    
+    for i in range(num):
+        if sum_branch == 0:
+            sum_branch = sum_tree(branches(t)[i])
+        else:
+            if sum_branch != sum_tree(branches(t)[i]):
+                return False
+        if not balanced(branches(t)[i]):
+            return False
+        
+    return True
+        
